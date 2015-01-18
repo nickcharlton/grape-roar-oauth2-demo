fa312a59512f2d3bc95aa4b6d22e731c65fcc531
Nick Charlton <nick@nickcharlton.net>
2015-01-18 21:01:00 +0000

    Improves the reloading helper.

--- config/initializers/reload_api.rb
+++ config/initializers/reload_api.rb
@@ -1,13 +1,11 @@
-api_dir = "#{Rails.root}/app/api"
-file_ext = 'rb'
+if Rails.env.development?
+  ActiveSupport::Dependencies.explicitly_unloadable_constants << "Twitter::API"
 
-api_files = Dir["#{api_dir}/**/*.#{file_ext}"]
-
-api_reloader = ActiveSupport::FileUpdateChecker.new(api_files, api_dir => file_ext) do
-  Rails.logger.info 'Reload routes for changed api files'
-  Rails.application.reload_routes!
-end
-
-ActionDispatch::Reloader.to_prepare do
-  api_reloader.execute_if_updated
+  api_files = Dir[Rails.root.join('app', 'api', '**', '*.rb')]
+  api_reloader = ActiveSupport::FileUpdateChecker.new(api_files) do
+    Rails.application.reload_routes!
+  end
+  ActionDispatch::Callbacks.to_prepare do
+    api_reloader.execute_if_updated
+  end
 end

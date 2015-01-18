b8a158a128dfd74b72113096305332e5f57c0be4
Nick Charlton <nick@nickcharlton.net>
2015-01-18 22:27:16 +0000

    Adds a method to create a new user.

--- app/api/routes/users.rb
+++ app/api/routes/users.rb
@@ -5,8 +5,22 @@ module Routes
       present User.all, with: UsersRepresenter
     end
 
+    desc 'Returns a specific user'
     get 'users/:id' do
       present User.find(params[:id]), with: UserRepresenter
+    end
+
+    desc 'Creates a new user'
+    params do
+      requires :user, type: Hash do
+        requires :name, type: String
+        optional :organisation_id, type: Integer
+      end
+    end
+    post 'users' do
+      user = create User, with: UserRepresenter, from: declared(params)[:user]
+
+      present user, with: UserRepresenter
     end
   end
 end

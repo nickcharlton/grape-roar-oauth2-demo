40db952c26c4e1a77a05535b1227ff23add4126f
Nick Charlton <nick@nickcharlton.net>
2015-01-20 12:17:44 +0000

    Adds Devise to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ require "#{Rails.root}/lib/hypermedia_extensions.rb"
 class User < ActiveRecord::Base
   include ::HypermediaExtensions
 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   belongs_to :organisation
 
   validates :name, presence: true

15aa6f2ea51e7f7715681093aae56bf64fc169d3
Nick Charlton <nick@nickcharlton.net>
2015-01-23 18:10:23 +0000

    Switches to use the Devise 'current_user' helper.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,7 +2,7 @@ Doorkeeper.configure do
   orm :active_record
 
   resource_owner_authenticator do
-    User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    current_user || redirect_to(new_user_session_url)
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.

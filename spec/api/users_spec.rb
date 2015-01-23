4851bab5459451efdda2cfb3f772ca574c66aafe
Nick Charlton <nick@nickcharlton.net>
2015-01-23 17:48:46 +0000

    Adds a spec to cover users requests w/data.

--- spec/api/users_spec.rb
+++ spec/api/users_spec.rb
@@ -1,11 +1,31 @@
 require 'rails_helper'
 
-describe Routes::Users do
-  describe 'without data' do
+RSpec.describe Routes::Users, type: :request do
+  context :without_data do
     it 'gives an empty list of users' do
       get '/users'
 
-      expect(response.body).to eq({ users: [] }.to_json)
+      expect(response.status).to eq 200
+      json = JSON.parse(response.body)
+
+      expect(json['users'].count).to eq 0
+    end
+  end
+
+  context :with_data do
+    it 'lists a user after one is added' do
+      user = create(:user)
+
+      get '/users'
+
+      expect(response.status).to eq 200
+      json = JSON.parse(response.body)
+
+      expect(json['users'].count).to eq 1
+      expect(json['users'].first['name']).to eq user.name
+
+      links = json['users'].first['_links']
+      expect(links['self']['href']).to include("/users/#{user.id}")
     end
   end
 end

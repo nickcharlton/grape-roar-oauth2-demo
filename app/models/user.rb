cc2d418cd15a683dbb25b213edd5de8d20e58631
Nick Charlton <nick@nickcharlton.net>
2015-01-23 13:05:28 +0000

    Provides the forward association for Applications from Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :oauth_applications,
+           class_name: 'Doorkeeper::Application', as: :owner
+
   belongs_to :organisation
 
   validates :name, presence: true

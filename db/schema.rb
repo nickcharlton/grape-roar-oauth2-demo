cdd8737bd80bf112c0a17dd546af69001865d1f6
Nick Charlton <nick@nickcharlton.net>
2015-01-23 13:02:24 +0000

    Requires that Applications be associated with Users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123124553) do
+ActiveRecord::Schema.define(version: 20150123125949) do
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", null: false
@@ -49,8 +49,11 @@ ActiveRecord::Schema.define(version: 20150123124553) do
     t.string   "scopes",       default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "owner_id"
+    t.string   "owner_type"
   end
 
+  add_index "oauth_applications", ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type"
   add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true
 
   create_table "organisations", force: :cascade do |t|

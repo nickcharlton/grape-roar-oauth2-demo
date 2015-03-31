b0bd2b92c76c6a542c82929bf1293b0ed3d64332
Nick Charlton <nick@nickcharlton.net>
2015-03-31 16:19:36 +0100

    Improves the error handling.
    This comes directly from a production service.

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,5 +3,20 @@ class API < Grape::API
   formatter :json, Grape::Formatter::Roar
   use ::WineBouncer::OAuth2
 
+  rescue_from WineBouncer::Errors::OAuthUnauthorizedError do |_e|
+    error = { id: 'unauthorized',
+              message: 'You must provide an OAuth token to complete this '\
+                       'request.' }
+
+    error_response(status: 401, message: error)
+  end
+
+  rescue_from WineBouncer::Errors::OAuthForbiddenError do |_e|
+    error = { id: 'forbidden',
+              message: 'This operation is not permitted with this scope.' }
+
+    error_response(status: 403, message: error)
+  end
+
   mount Routes::Users
 end
